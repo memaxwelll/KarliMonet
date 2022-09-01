af0707e15e8a999db84119876598b41bee332f6f
ThaBluNate <epicgamernate@gmail.com>
2022-08-31 20:09:49 -0400

    2022 demo v2
    Added stuff

--- site.js
+++ site.js
@@ -1,37 +1,55 @@
-sndv=0;Reloaded=0;Monet=0;reloadGifs();setInterval(reloadGifs, 4000);
+sndv=0;var Reloaded=0;var Monet=0;time=4000
+timer=setInterval(reloadGifs, time)
+chTimer()
 
-//PWA
-if('serviceWorker' in navigator){navigator.serviceWorker.register('./sw.js')}
+if('serviceWorker' in navigator){navigator.serviceWorker.register('./sw.js')} //PWA setup
 
-function snd(){
+function githubPage(){window.location.href="https://github.com/ThaBluNate/ClassicMonet"}
+
+function snd(){ //This runs if you click the sound button
     if(sndv==0){
-        console.debug('[SND] Playing!')
         document.getElementById('sound').style.backgroundImage="url('./assets/sound.png')"
         document.getElementById("Audio").play()
         sndv=1
     }else{
-        console.debug('[SND] Pausing!')
         document.getElementById('sound').style.backgroundImage="url('./assets/mute.png')"
         document.getElementById("Audio").pause()
         sndv=0
     }
 }
-function reloadGifs(){
-    console.debug('[GIF] Reloading!')
-    if(Monet==1){
-        if(Reloaded==1){
+function settings(oc){if(oc=='open'){document.getElementById('setpanel').className=""}else{document.getElementById('setpanel').className="invis"}}
+document.getElementById('slide').oninput=function(){reloadGifs(this.value)}
+function chTimer(){
+    time=document.getElementById('text').value
+    document.getElementById('MS').innerHTML=time+" MS"
+    sec=Math.round(time/1000)
+    document.getElementById('Sec').innerHTML=sec+" Sec"
+    reTimer()
+}
+function reTimer(){
+    reloadGifs()
+    clearInterval(timer);
+    timer=setInterval(reloadGifs, time);
+}
+function reloadGifs(gifset){
+    if(gifset==1){window.Monet=1} //Cachemonet gifs only
+    if(gifset==2){window.Monet=0;window.Reloaded=1} //Cachemonet+CMReloaded
+    if(gifset==3){window.Monet=0;window.Reloaded=0} //CacheMonet+CMReloaded+ClassicMonet
+    if(window.Monet==1){
+        document.getElementById('GIFs').innerHTML="CacheMonet"
+        rand1=Math.floor(Math.random()*53)+1
+        rand2=Math.floor(Math.random()*38)+1
+    }else{
+        if(window.Reloaded==1){
+            document.getElementById('GIFs').innerHTML="CM-Reloaded"
             rand1=Math.floor(Math.random()*85)+1
             rand2=Math.floor(Math.random()*58)+1
         }else{
-            rand1=Math.floor(Math.random()*53)+1
-            rand2=Math.floor(Math.random()*38)+1
-        }
-    }else{
+        document.getElementById('GIFs').innerHTML="ClassicMonet"
         rand1=Math.floor(Math.random()*105)+1
         rand2=Math.floor(Math.random()*95)+1
+        }
     }
-    console.debug('[GIF] Random FG = '+rand1)
-    console.debug('[GIF] Random BG = '+rand2)
     document.getElementById('center').src="./assets/FG/"+rand1+".gif"
     document.body.style.backgroundImage="url(./assets/BG/"+rand2+".gif)"
 }
