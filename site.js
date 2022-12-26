3d1a7910663ebf14150fa2cf98c4a431d7051563
ThaBluNate <epicgamernate@gmail.com>
2022-12-26 02:49:49 -0500

    2022 RC1
    Update files for RC1

--- site.js
+++ site.js
@@ -1,13 +1,5 @@
-sndv=0;var Reloaded=0;var Monet=0;time=4000
-document.getElementById('slide').value=cookget('gif')
-document.getElementById('text').value=cookget('time')
-time=cookget('time')
-timer=setInterval(reloadGifs, time)
-chTimer(cookget('gif'))
-
 if('serviceWorker' in navigator){navigator.serviceWorker.register('./sw.js')} //PWA setup
-
-function githubPage(){window.location.href="https://github.com/ThaBluNate/ClassicMonet"}
+sndv=0
 
 function snd(){ //This runs if you click the sound button
     if(sndv==0){
@@ -21,46 +13,89 @@ function snd(){ //This runs if you click the sound button
     }
 }
 
-// Settings
-function settings(oc){if(oc=='open'){document.getElementById('setpanel').className=""}else{document.getElementById('setpanel').className="invis"}}
-document.getElementById('slide').oninput=function(){reloadGifs(this.value)}
-
-// settings buttons
-function md(v){if(v==1){document.getElementById('del').src='./assets/pdel.png'}else{document.getElementById('sav').src='./assets/psave.png'}}
-function mu(v){if(v==1){document.getElementById('del').src='./assets/del.png'}else{document.getElementById('sav').src='./assets/save.png'}}
-
-function chTimer(v){
-    time=document.getElementById('text').value
-    document.getElementById('MS').innerHTML=time+" MS"
-    sec=Math.round(time/1000)
-    document.getElementById('Sec').innerHTML=sec+" Sec"
-    reTimer(v)
-}
-function reTimer(v){
-    reloadGifs(v)
-    clearInterval(timer);
-    timer=setInterval(reloadGifs, time);
-}
-
-function reloadGifs(gifset){
-    switch(gifset){
-        case '1': window.Monet=1;break //Cachemonet gifs only
-        case '2': window.Monet=0;window.Reloaded=1;break //Cachemonet+CMReloaded
-        case '3': window.Monet=0;window.Reloaded=0 //CacheMonet+CMReloaded+ClassicMonet
-        default:
+function checkLocal(L,R){ //Check if localstorage has L, if not, set L to R
+    if (localStorage.getItem(L)==null){
+        localStorage.setItem(L,R)
     }
-    if(window.Monet==1){
-        document.getElementById('GIFs').innerHTML="CacheMonet"
-        rand1=Math.floor(Math.random()*53)+1
-        rand2=Math.floor(Math.random()*38)+1
-    }else if(window.Reloaded==1){
-        document.getElementById('GIFs').innerHTML="CM-Reloaded"
-        rand1=Math.floor(Math.random()*85)+1
-        rand2=Math.floor(Math.random()*59)+1
+}
+
+function settings(oc){ //Show and hide settings panel
+    if(oc=='open'){
+        document.getElementById('setpanel').className=""
     }else{
-        document.getElementById('GIFs').innerHTML="ClassicMonet"
-        rand1=Math.floor(Math.random()*105)+1
-        rand2=Math.floor(Math.random()*96)+1
+        document.getElementById('setpanel').className="invis"
+    }
+}
+
+function save(){ //exexutes when you click the "save" button
+    localStorage.setItem('gif',document.getElementById('slide').value)
+    localStorage.setItem('time',document.getElementById('text').value)
+    localStorage.setItem('img',document.getElementById('slide2').value)
+}
+
+function githubPage(){window.location.href="https://github.com/ThaBluNate/ClassicMonet"} //change page to github
+
+function wipe(){localStorage.clear()} //exexutes when you click the "wipe" button, just clears all settings
+
+function reTimer(v){ //reset the GIF timer and call reloadGifs() + gif()
+    reloadGifs(v)
+    gif()
+    try{clearInterval(timer)}catch{}
+    time=document.getElementById('text').value
+    timer=setInterval(reloadGifs, time)
+}
+
+function gif(){ //set the center image type (Stretch, Original)
+    if(document.getElementById('slide2').value==2){
+        document.getElementById("center").style="width:100vw;height:76vh;object-fit:contain"
+        document.getElementById("Cimg").innerHTML="Stretch"
+    }else{
+        document.getElementById("center").style=""
+        document.getElementById("Cimg").innerHTML="Original"
+    }
+}
+
+//check if settings are set in LocalStorage, if not, set them ()
+checkLocal('gif','3')
+checkLocal('time','4000')
+checkLocal('img','2')
+
+//Take settings from LocalStorage and apply them
+document.getElementById('slide').value=localStorage.getItem('gif')
+document.getElementById('text').value=localStorage.getItem('time')
+document.getElementById('slide2').value=localStorage.getItem('img')
+
+//Slider code
+document.getElementById('slide').oninput=function(){reloadGifs(this.value)}
+document.getElementById('text').oninput=function(){reTimer()}
+document.getElementById('slide2').oninput=function(){gif()}
+
+//Press "animation" on save/wipe buttons
+document.getElementById('del').onmousedown=function(){document.getElementById('del').src="./assets/pdel.png"}
+document.getElementById('del').onmouseup=function(){document.getElementById('del').src="./assets/del.png"}
+document.getElementById('sav').onmousedown=function(){document.getElementById('sav').src="./assets/psave.png"}
+document.getElementById('sav').onmouseup=function(){document.getElementById('sav').src="./assets/save.png"}
+
+//Start the site
+reTimer(localStorage.getItem('gif'))
+
+//Displays the GIFs onscreen, If gifSet is defined, it will change how many gifs it uses
+function reloadGifs(){
+    switch(document.getElementById('slide').value){
+        case '1':
+            document.getElementById('GIFs').innerHTML="CacheMonet"
+            rand1=Math.floor(Math.random()*53)+1
+            rand2=Math.floor(Math.random()*38)+1
+            break
+        case '2':
+            document.getElementById('GIFs').innerHTML="CM-Reloaded"
+            rand1=Math.floor(Math.random()*85)+1
+            rand2=Math.floor(Math.random()*59)+1
+            break
+        case '3':
+            document.getElementById('GIFs').innerHTML="ClassicMonet"
+            rand1=Math.floor(Math.random()*105)+1
+            rand2=Math.floor(Math.random()*96)+1
     }
     document.getElementById('center').src="./assets/FG/"+rand1+".gif"
     document.body.style.backgroundImage="url(./assets/BG/"+rand2+".gif)"
--- sw.js
+++ sw.js
@@ -1,42 +1,44 @@
 const cacheName = 'offline';
-CacheData = [
-"./",
-"./manifest.json",
-"./index.html",
-"./site.css",
-"./site.js",
-"./cook'e.js",
-"./assets/windowdipper.mp3",
-"./assets/sound.png",
-"./assets/mute.png",
-"./assets/git.png",
-"./assets/gif.png",
-"./assets/save.png",
-"./assets/del.png",
-"./assets/psave.png",
-"./assets/pdel.png",
-"./assets/cur.png",
-"./assets/set.png",
-"./assets/close.png",
-"./assets/icons/icon.png",
-"./assets/icons/icon192.png",
-"./assets/icons/icon512.png",
-"./assets/icons/iconmask.png",
-"./assets/FG/1.gif","./assets/FG/2.gif","./assets/FG/3.gif","./assets/FG/4.gif","./assets/FG/5.gif","./assets/FG/6.gif","./assets/FG/7.gif","./assets/FG/8.gif","./assets/FG/9.gif","./assets/FG/10.gif","./assets/FG/11.gif","./assets/FG/12.gif","./assets/FG/13.gif","./assets/FG/14.gif","./assets/FG/15.gif","./assets/FG/16.gif","./assets/FG/17.gif","./assets/FG/18.gif","./assets/FG/19.gif","./assets/FG/20.gif","./assets/FG/21.gif","./assets/FG/22.gif","./assets/FG/23.gif","./assets/FG/24.gif","./assets/FG/25.gif","./assets/FG/26.gif","./assets/FG/27.gif","./assets/FG/28.gif","./assets/FG/29.gif","./assets/FG/30.gif","./assets/FG/31.gif","./assets/FG/32.gif","./assets/FG/33.gif","./assets/FG/34.gif","./assets/FG/35.gif","./assets/FG/36.gif","./assets/FG/37.gif","./assets/FG/38.gif","./assets/FG/39.gif","./assets/FG/40.gif","./assets/FG/41.gif","./assets/FG/42.gif","./assets/FG/43.gif","./assets/FG/44.gif","./assets/FG/45.gif","./assets/FG/46.gif","./assets/FG/47.gif","./assets/FG/48.gif","./assets/FG/49.gif","./assets/FG/50.gif","./assets/FG/51.gif","./assets/FG/52.gif","./assets/FG/53.gif","./assets/FG/54.gif","./assets/FG/55.gif","./assets/FG/56.gif","./assets/FG/57.gif","./assets/FG/58.gif","./assets/FG/59.gif","./assets/FG/60.gif","./assets/FG/61.gif","./assets/FG/62.gif","./assets/FG/63.gif","./assets/FG/64.gif","./assets/FG/65.gif","./assets/FG/66.gif","./assets/FG/67.gif","./assets/FG/68.gif","./assets/FG/69.gif","./assets/FG/70.gif","./assets/FG/71.gif","./assets/FG/72.gif","./assets/FG/73.gif","./assets/FG/74.gif","./assets/FG/75.gif","./assets/FG/76.gif","./assets/FG/77.gif","./assets/FG/78.gif","./assets/FG/79.gif","./assets/FG/80.gif","./assets/FG/81.gif","./assets/FG/82.gif","./assets/FG/83.gif","./assets/FG/84.gif","./assets/FG/85.gif","./assets/FG/86.gif","./assets/FG/87.gif","./assets/FG/88.gif","./assets/FG/89.gif","./assets/FG/90.gif","./assets/FG/91.gif","./assets/FG/92.gif","./assets/FG/93.gif","./assets/FG/94.gif","./assets/FG/95.gif","./assets/FG/96.gif","./assets/FG/97.gif","./assets/FG/98.gif","./assets/FG/99.gif","./assets/FG/100.gif","./assets/FG/101.gif","./assets/FG/102.gif","./assets/FG/103.gif","./assets/FG/104.gif","./assets/FG/105.gif",
-"./assets/BG/1.gif","./assets/BG/2.gif","./assets/BG/3.gif","./assets/BG/4.gif","./assets/BG/5.gif","./assets/BG/6.gif","./assets/BG/7.gif","./assets/BG/8.gif","./assets/BG/9.gif","./assets/BG/10.gif","./assets/BG/11.gif","./assets/BG/12.gif","./assets/BG/13.gif","./assets/BG/14.gif","./assets/BG/15.gif","./assets/BG/16.gif","./assets/BG/17.gif","./assets/BG/18.gif","./assets/BG/19.gif","./assets/BG/20.gif","./assets/BG/21.gif","./assets/BG/22.gif","./assets/BG/23.gif","./assets/BG/24.gif","./assets/BG/25.gif","./assets/BG/26.gif","./assets/BG/27.gif","./assets/BG/28.gif","./assets/BG/29.gif","./assets/BG/30.gif","./assets/BG/31.gif","./assets/BG/32.gif","./assets/BG/33.gif","./assets/BG/34.gif","./assets/BG/35.gif","./assets/BG/36.gif","./assets/BG/37.gif","./assets/BG/38.gif","./assets/BG/39.gif","./assets/BG/40.gif","./assets/BG/41.gif","./assets/BG/42.gif","./assets/BG/43.gif","./assets/BG/44.gif","./assets/BG/45.gif","./assets/BG/46.gif","./assets/BG/47.gif","./assets/BG/48.gif","./assets/BG/49.gif","./assets/BG/50.gif","./assets/BG/51.gif","./assets/BG/52.gif","./assets/BG/53.gif","./assets/BG/54.gif","./assets/BG/55.gif","./assets/BG/56.gif","./assets/BG/57.gif","./assets/BG/58.gif","./assets/BG/59.gif","./assets/BG/60.gif","./assets/BG/61.gif","./assets/BG/62.gif","./assets/BG/63.gif","./assets/BG/64.gif","./assets/BG/65.gif","./assets/BG/66.gif","./assets/BG/67.gif","./assets/BG/68.gif","./assets/BG/69.gif","./assets/BG/70.gif","./assets/BG/71.gif","./assets/BG/72.gif","./assets/BG/73.gif","./assets/BG/74.gif","./assets/BG/75.gif","./assets/BG/76.gif","./assets/BG/77.gif","./assets/BG/78.gif","./assets/BG/79.gif","./assets/BG/80.gif","./assets/BG/81.gif","./assets/BG/82.gif","./assets/BG/83.gif","./assets/BG/84.gif","./assets/BG/85.gif","./assets/BG/86.gif","./assets/BG/87.gif","./assets/BG/88.gif","./assets/BG/89.gif","./assets/BG/90.gif","./assets/BG/91.gif","./assets/BG/92.gif","./assets/BG/93.gif","./assets/BG/94.gif","./assets/BG/95.gif","./assets/BG/96.gif"
+CacheData=[
+  "./",
+  "./manifest.json",
+  "./index.html",
+  "./site.css",
+  "./site.js",
+  "./assets/windowdipper.mp3",
+  "./assets/sound.png",
+  "./assets/mute.png",
+  "./assets/git.png",
+  "./assets/gif.png",
+  "./assets/save.png",
+  "./assets/del.png",
+  "./assets/psave.png",
+  "./assets/pdel.png",
+  "./assets/cur.png",
+  "./assets/set.png",
+  "./assets/close.png",
+  "./assets/icons/icon.png",
+  "./assets/icons/icon192.png",
+  "./assets/icons/icon512.png",
+  "./assets/icons/iconmask.png",
+  "./assets/FG/1.gif","./assets/FG/2.gif","./assets/FG/3.gif","./assets/FG/4.gif","./assets/FG/5.gif","./assets/FG/6.gif","./assets/FG/7.gif","./assets/FG/8.gif","./assets/FG/9.gif","./assets/FG/10.gif","./assets/FG/11.gif","./assets/FG/12.gif","./assets/FG/13.gif","./assets/FG/14.gif","./assets/FG/15.gif","./assets/FG/16.gif","./assets/FG/17.gif","./assets/FG/18.gif","./assets/FG/19.gif","./assets/FG/20.gif","./assets/FG/21.gif","./assets/FG/22.gif","./assets/FG/23.gif","./assets/FG/24.gif","./assets/FG/25.gif","./assets/FG/26.gif","./assets/FG/27.gif","./assets/FG/28.gif","./assets/FG/29.gif","./assets/FG/30.gif","./assets/FG/31.gif","./assets/FG/32.gif","./assets/FG/33.gif","./assets/FG/34.gif","./assets/FG/35.gif","./assets/FG/36.gif","./assets/FG/37.gif","./assets/FG/38.gif","./assets/FG/39.gif","./assets/FG/40.gif","./assets/FG/41.gif","./assets/FG/42.gif","./assets/FG/43.gif","./assets/FG/44.gif","./assets/FG/45.gif","./assets/FG/46.gif","./assets/FG/47.gif","./assets/FG/48.gif","./assets/FG/49.gif","./assets/FG/50.gif","./assets/FG/51.gif","./assets/FG/52.gif","./assets/FG/53.gif","./assets/FG/54.gif","./assets/FG/55.gif","./assets/FG/56.gif","./assets/FG/57.gif","./assets/FG/58.gif","./assets/FG/59.gif","./assets/FG/60.gif","./assets/FG/61.gif","./assets/FG/62.gif","./assets/FG/63.gif","./assets/FG/64.gif","./assets/FG/65.gif","./assets/FG/66.gif","./assets/FG/67.gif","./assets/FG/68.gif","./assets/FG/69.gif","./assets/FG/70.gif","./assets/FG/71.gif","./assets/FG/72.gif","./assets/FG/73.gif","./assets/FG/74.gif","./assets/FG/75.gif","./assets/FG/76.gif","./assets/FG/77.gif","./assets/FG/78.gif","./assets/FG/79.gif","./assets/FG/80.gif","./assets/FG/81.gif","./assets/FG/82.gif","./assets/FG/83.gif","./assets/FG/84.gif","./assets/FG/85.gif","./assets/FG/86.gif","./assets/FG/87.gif","./assets/FG/88.gif","./assets/FG/89.gif","./assets/FG/90.gif","./assets/FG/91.gif","./assets/FG/92.gif","./assets/FG/93.gif","./assets/FG/94.gif","./assets/FG/95.gif","./assets/FG/96.gif","./assets/FG/97.gif","./assets/FG/98.gif","./assets/FG/99.gif","./assets/FG/100.gif","./assets/FG/101.gif","./assets/FG/102.gif","./assets/FG/103.gif","./assets/FG/104.gif","./assets/FG/105.gif",
+  "./assets/BG/1.gif","./assets/BG/2.gif","./assets/BG/3.gif","./assets/BG/4.gif","./assets/BG/5.gif","./assets/BG/6.gif","./assets/BG/7.gif","./assets/BG/8.gif","./assets/BG/9.gif","./assets/BG/10.gif","./assets/BG/11.gif","./assets/BG/12.gif","./assets/BG/13.gif","./assets/BG/14.gif","./assets/BG/15.gif","./assets/BG/16.gif","./assets/BG/17.gif","./assets/BG/18.gif","./assets/BG/19.gif","./assets/BG/20.gif","./assets/BG/21.gif","./assets/BG/22.gif","./assets/BG/23.gif","./assets/BG/24.gif","./assets/BG/25.gif","./assets/BG/26.gif","./assets/BG/27.gif","./assets/BG/28.gif","./assets/BG/29.gif","./assets/BG/30.gif","./assets/BG/31.gif","./assets/BG/32.gif","./assets/BG/33.gif","./assets/BG/34.gif","./assets/BG/35.gif","./assets/BG/36.gif","./assets/BG/37.gif","./assets/BG/38.gif","./assets/BG/39.gif","./assets/BG/40.gif","./assets/BG/41.gif","./assets/BG/42.gif","./assets/BG/43.gif","./assets/BG/44.gif","./assets/BG/45.gif","./assets/BG/46.gif","./assets/BG/47.gif","./assets/BG/48.gif","./assets/BG/49.gif","./assets/BG/50.gif","./assets/BG/51.gif","./assets/BG/52.gif","./assets/BG/53.gif","./assets/BG/54.gif","./assets/BG/55.gif","./assets/BG/56.gif","./assets/BG/57.gif","./assets/BG/58.gif","./assets/BG/59.gif","./assets/BG/60.gif","./assets/BG/61.gif","./assets/BG/62.gif","./assets/BG/63.gif","./assets/BG/64.gif","./assets/BG/65.gif","./assets/BG/66.gif","./assets/BG/67.gif","./assets/BG/68.gif","./assets/BG/69.gif","./assets/BG/70.gif","./assets/BG/71.gif","./assets/BG/72.gif","./assets/BG/73.gif","./assets/BG/74.gif","./assets/BG/75.gif","./assets/BG/76.gif","./assets/BG/77.gif","./assets/BG/78.gif","./assets/BG/79.gif","./assets/BG/80.gif","./assets/BG/81.gif","./assets/BG/82.gif","./assets/BG/83.gif","./assets/BG/84.gif","./assets/BG/85.gif","./assets/BG/86.gif","./assets/BG/87.gif","./assets/BG/88.gif","./assets/BG/89.gif","./assets/BG/90.gif","./assets/BG/91.gif","./assets/BG/92.gif","./assets/BG/93.gif","./assets/BG/94.gif","./assets/BG/95.gif","./assets/BG/96.gif"
 ];
 
-self.addEventListener('install', function(event) {
-  event.waitUntil((async () => {
-    caches.open(cacheName).then((cache) => cache.addAll(CacheData))
-  })());
-});
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open("offline").then(cache => {
+      cache.addAll(CacheData)
+    })
+  )
+})
+
+self.addEventListener('activate', event => {
   self.clients.claim();
-});
+})
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
   event.waitUntil((async () => {
     try{
       event.respondWith(
